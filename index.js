c731d317866572513b8165a6f26cb12d3ff0fba7
mikesila <michaelsila97@gmail.com>
2023-03-20 07:33:45 +0300

    Done with Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function logTwoValues(value1, value2){
+    console.log(`The two values are ${value1} and ${value2}.`);
+}
+function introductionWithLanguage(name, language){
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+function sayHelloTo(firstName){
+    console.log(`Hello, ${firstName}!`);
+}
+function sayHelloTo(firstName="User"){
+    console.log(`Hello, ${firstName}!`);
+}
